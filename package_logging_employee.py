aa81fff4d403585925625f9817ab020261809501
toanbui1991 <toanbui1991@gmail.com>
2022-05-06 15:44:47 +0700

    add command_ssh

--- package_logging_employee.py
+++ package_logging_employee.py
@@ -4,7 +4,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-
 file_handler = logging.FileHandler('employee.log')
 file_handler.setFormatter(formatter)
 
