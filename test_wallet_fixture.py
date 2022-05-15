8cbf09ea31572c2267ced0c90ec879191bfb20c9
toanbui1991 <toanbui1991@gmail.com>
2022-05-15 14:55:04 +0700

    add pytest

---
+++ test_wallet_fixture.py
@@ -0,0 +1,49 @@
+import pytest
+from wallet import Wallet, InsufficientAmount
+
+@pytest.fixture
+def empty_wallet():
+    '''Returns a Wallet instance with a zero balance'''
+    return Wallet()
+
+@pytest.fixture
+def wallet():
+    '''Returns a Wallet instance with a balance of 20'''
+    return Wallet(20)
+
+@pytest.fixture
+def my_wallet():
+    
+    return Wallet()
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+def test_setting_initial_amount(wallet):
+    assert wallet.balance == 20
+
+def test_wallet_add_cash(wallet):
+    wallet.add_cash(80)
+    assert wallet.balance == 100
+
+def test_wallet_spend_cash(wallet):
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
+
+def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
+    with pytest.raises(InsufficientAmount):
+        empty_wallet.spend_cash(100)
+
+
+#use fixture and parametrize together
+
+#use parametrize
+@pytest.mark.parametrize("earned,spent,expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+]) #have to put my_wallet instance into the test function when combine fixture and parametrize
+#when combine fixture and parametrize we have to input class instance to the test function.
+def test_transactions(my_wallet, earned, spent, expected):
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
