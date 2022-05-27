df3e2b470708a22eea2b6ccc7f58a3377c860bf7
root <root@AC929999.localdomain>
2022-05-27 08:46:03 +0700

    update config.py

--- config.py
+++ config.py
@@ -2,7 +2,14 @@ config = {
     #config with default db
     'MYSQL_DB': {'host': 'localhost', 'port': 3306, 'user': 'root', 'password': 'Buixuantoan@916263', 'db': 'northwind'},
     'PG_DB': {'host': 'localhost', 'port': '5433', 'user': 'postgres', 'password': 'Buixuantoan@916263'},
-    'MSSQL_DB': {'host': 'localhost', 'port': 1433, 'user': 'sa', 'password': 'Buixuantoan@916263', 'database': 'Northwind'}
+    'MSSQL_DB': {'host': 'localhost', 'port': 1433, 'user': 'sa', 'password': 'Buixuantoan@916263', 'database': 'Northwind'},
+    'mysql_dev': {
+        'user': 'root',
+        'passwd': 'Buixuantoan916263',
+        'host': 'localhost',
+        'port': 3306,
+        'db': 'review_dev',
+        'database': 'review_dev'},
 }
 
 test = ''
