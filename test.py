27c222adb93342fc69ce10d741e103e86983a172
toanbui1991 <toanbui1991@gmail.com>
2022-10-09 10:59:17 +0700

    update

---
+++ test.py
@@ -0,0 +1,8 @@
+path: "domain_name.com/order?id=123"
+method: "patch"
+
+path: "domain_name.com/?post_id=123&comment_id:3455"
+method: "update"
+
+path: "domain_name.com/?product_id=123&basked_id=678"
+method: "patch"
